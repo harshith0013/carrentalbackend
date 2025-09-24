833c5da6caefeb4d047fafcc2d86edade744e548
harshith0013 <kuppuruharshith@gmail.com>
2025-09-24 22:33:28 +0530

    Update CarRentalApplication.java

--- src/main/java/com/example/carrental/CarRentalApplication.java
+++ src/main/java/com/example/carrental/CarRentalApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.carrental")
