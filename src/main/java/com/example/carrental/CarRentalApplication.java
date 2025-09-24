edb6b83edef9157f9313998a16b6e4ca12b7ab38
harshith0013 <kuppuruharshith@gmail.com>
2025-09-24 22:38:30 +0530

    Update CarRentalApplication.java

--- src/main/java/com/example/carrental/CarRentalApplication.java
+++ src/main/java/com/example/carrental/CarRentalApplication.java
@@ -8,12 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.carrental")
 @EntityScan(basePackages = "com.example.carrental.model")
 @EnableJpaRepositories(basePackages = "com.example.carrental.repository")
-@SpringBootApplication
 public class CarRentalApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
